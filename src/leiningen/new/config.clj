93b5cda6fcb2f7a86f1fca82c68b2079ba133d64
Alexei Guevara <alexguev@gmail.com>
2013-04-02 23:52:39 -0400

    put in Dmitri's simple solution

--- src/leiningen/new/config.clj
+++ src/leiningen/new/config.clj
@@ -1,61 +1,49 @@
 (ns luminus.config)
 
-(def dependencies [:+site [:+clabango [:+auth-db 
-                                       :+dailycred]
-                           :+hiccup [:+auth-db 
-                                     :+dailycred]]])
+(defn handler [name path] (println name path))
 
-(defn to-map
-  ([coll] 
-   (apply to-map coll))
-  ([k v]
-   (if (vector? v)
-     (array-map k (apply to-map v))
-     (array-map k nil, v nil)))
-  ([k v & more]
-   (merge (apply to-map more)
-          (to-map k v))))
+(def dependency-tree 
+  {:deps
+   [{:name :+site
+     :handler handler
+     :default true
+     :deps [{:name :+clabango
+             :handler handler
+             :default true
+             :deps [{:name :+auth-db
+                     :handler handler
+                     :default true}
+                    {:name :+auth-dailycred
+                     :handler handler}]}
+            {:name :+hiccup
+             :handler handler
+             :deps [{:name :+auth-db
+                     :handler handler
+                     :default true}
+                    {:name :+auth-dailycred
+                     :handler handler}]}]}]})
 
-(defn select-dep [deps feats]
-  (when (seq deps)
-    (or (some (set (keys deps)) feats)
-        (first (keys deps)))))
-  
-(defn path [feature deps]
-  (when (contains? deps feature)
-    (loop [path []
-           feat feature]
-      (if-let [parent (some (fn [[k v]] 
-                           (when (and (map? v) (some #{feat} (keys v))) 
-                             k))
-                         (seq deps))]
-        (recur (into [parent] path) parent)
-        path))))
+(defn filter-deps [features deps]
+  (or (not-empty (filter #(some #{(:name %)} features) deps))
+      (filter :default deps)))
 
-(defn expand [features]
-  "a convenient property of this function is that, after expanding, features will be sorted by order or execution"
-  (loop [feats (seq features)
-         deps (to-map dependencies)
-         result []]
-    (if (empty? feats)
-      result
-      (let [feat (first feats)
-            feat-deps (get deps feat)
-            selected-dep (select-dep feat-deps (concat feats result))
-            new-feats (if selected-dep 
-                        (cons selected-dep (rest feats)) 
-                        (rest feats)) 
-            new-deps (->> [selected-dep] (select-keys feat-deps) (merge deps))
-            new-result (-> #{feat} (remove result) vec (conj feat))]
-        (recur new-feats
-               new-deps
-               new-result)))))
+(defn prune [features]
+  (clojure.walk/prewalk
+   (fn [item]
+     (if (map? item)
+       (update-in item [:deps] (partial filter-deps features))
+       item))     
+   dependency-tree))
 
-(defn augment-with-path [features]
-  (second (reduce (fn [[deps result] feat] 
-                    [(merge deps (select-keys (get deps feat) features)) 
-                     (conj result [feat (path feat deps)])]) 
-                  [(to-map dependencies) []] 
-                  features)))
+(defn process-features 
+  ([features] (add-features [] features))
+  ([path features]
+   (doseq [{:keys [handler name deps]} features]
+     (if handler (handler name path))
+     (add-features (conj path name) deps)
+   )))
 
-;(augment-with-path [:+site :+clabango :+dailycred])
+(->> [:+site :+auth-dailycred] prune :deps process-features)
+
+
+(prune [:+site])
